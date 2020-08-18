a45b5e709e0f84bddc2ee6d82e2135f0ee087767
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-08-18 20:10:14 +0900

    feat: 時刻が確定してので変更

--- function.go
+++ function.go
@@ -152,7 +152,7 @@ func getTargetDate() time.Time {
 
 func getTargetDateTime() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 8, 22, 13, 0, 0, 0, jst)
+	return time.Date(2020, 8, 22, 14, 0, 0, 0, jst)
 }
 
 func getNow() time.Time {
