d18be61497b84d337249a903f3db550d4b82fcfe
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-09-06 02:22:08 +0900

    feat: 新たなイベントを設定

--- function.go
+++ function.go
@@ -147,12 +147,12 @@ func selectPOPSong(t time.Time) (SongInfo, string) {
 
 func getTargetDate() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 9, 5, 0, 0, 0, 0, jst)
+	return time.Date(2020, 9, 6, 0, 0, 0, 0, jst)
 }
 
 func getTargetDateTime() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 9, 5, 12, 0, 0, 0, jst)
+	return time.Date(2020, 9, 6, 12, 0, 0, 0, jst)
 }
 
 func getNow() time.Time {
@@ -315,8 +315,8 @@ func main() {
 		textTw = "まもなく 1stアルバム『POP』発売記念 インターネットサイン会!!"
 		out = generateTodayImage(selectRandomImage(), text)
 	} else {
-		text = fmt.Sprintf("2020/09/05\n1stアルバム『POP』発売記念\nインターネットサイン会まで\n%s!!", countdownText(now))
-		textTw = fmt.Sprintf("2020/09/05 1stアルバム『POP』発売記念 インターネットサイン会まで%s!!", countdownText(now))
+		text = fmt.Sprintf("2020/09/06\n1stアルバム『POP』発売記念\nインターネットサイン会まで\n%s!!", countdownText(now))
+		textTw = fmt.Sprintf("2020/09/06 1stアルバム『POP』発売記念 インターネットサイン会まで%s!!", countdownText(now))
 		out = generateTodayImage(selectRandomImage(), text)
 	}
 	// encode image to base64
