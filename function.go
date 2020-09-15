93b558daea72305b68639629a809413a4907f684
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-09-16 03:13:37 +0900

    feat: 新たなイベントを設定

--- function.go
+++ function.go
@@ -147,12 +147,12 @@ func selectPOPSong(t time.Time) (SongInfo, string) {
 
 func getTargetDate() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 9, 21, 0, 0, 0, 0, jst)
+	return time.Date(2020, 9, 19, 0, 0, 0, 0, jst)
 }
 
 func getTargetDateTime() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 9, 21, 14, 0, 0, 0, jst)
+	return time.Date(2020, 9, 19, 17, 0, 0, 0, jst)
 }
 
 func getNow() time.Time {
@@ -311,12 +311,12 @@ func main() {
 	var text string
 	var textTw string
 	if near {
-		text = "まもなく 1stアルバム『POP』発売記念\nインターネットサイン会!!"
-		textTw = "まもなく 1stアルバム『POP』発売記念 インターネットサイン会!!"
+		text = "まもなく 「POP」発売記念\nTalkportオンラインリリースイベント!!"
+		textTw = "まもなく 「POP」発売記念Talkportオンラインリリースイベント!!"
 		out = generateTodayImage(selectRandomImage(), text)
 	} else {
-		text = fmt.Sprintf("2020/09/21\n1stアルバム『POP』発売記念\nインターネットサイン会まで\n%s!!", countdownText(now))
-		textTw = fmt.Sprintf("2020/09/21 1stアルバム『POP』発売記念 インターネットサイン会まで%s!!", countdownText(now))
+		text = fmt.Sprintf("2020/09/19\n「POP」発売記念\nTalkportオンラインリリースイベントまで\n%s!!", countdownText(now))
+		textTw = fmt.Sprintf("2020/09/19 「POP」発売記念Talkportオンラインリリースイベントまで%s!!", countdownText(now))
 		out = generateTodayImage(selectRandomImage(), text)
 	}
 	// encode image to base64
