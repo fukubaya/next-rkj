ec737f0fc4352331d3bc620111d25c19f42cef2b
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-11-15 01:26:18 +0900

    feat: 新たなイベントを設定

--- function.go
+++ function.go
@@ -147,12 +147,12 @@ func selectPOPSong(t time.Time) (SongInfo, string) {
 
 func getTargetDate() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 11, 14, 0, 0, 0, 0, jst)
+	return time.Date(2020, 11, 15, 0, 0, 0, 0, jst)
 }
 
 func getTargetDateTime() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 11, 14, 13, 0, 0, 0, jst)
+	return time.Date(2020, 11, 15, 15, 0, 0, 0, jst)
 }
 
 func getNow() time.Time {
@@ -311,12 +311,12 @@ func main() {
 	var text string
 	var textTw string
 	if near {
-		text = "まもなく\n1st single「Don’t Blink」発売記念\nインターネットサイン会！"
-		textTw = "まもなく1st single 「Don’t Blink」発売記念インターネットサイン会！"
+		text = "まもなく\n1st single「Don’t Blink」発売記念\nTalk Portトーク特典会！"
+		textTw = "まもなく1st single 「Don’t Blink」発売記念Talk Portトーク特典会！"
 		out = generateTodayImage(selectRandomImage(), text)
 	} else {
-		text = fmt.Sprintf("2020/11/14\n1st single「Don’t Blink」発売記念\nインターネットサイン会まで\n%s!!", countdownText(now))
-		textTw = fmt.Sprintf("2020/11/14 1st single「Don’t Blink」発売記念インターネットサイン会まで%s!!", countdownText(now))
+		text = fmt.Sprintf("2020/11/15\n1st single「Don’t Blink」発売記念\nTalk Portトーク特典会まで\n%s!!", countdownText(now))
+		textTw = fmt.Sprintf("2020/11/15 1st single「Don’t Blink」発売記念Talk Portトーク特典会まで%s!!", countdownText(now))
 		out = generateTodayImage(selectRandomImage(), text)
 	}
 	// encode image to base64
