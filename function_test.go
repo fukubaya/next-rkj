eac2dda94e8477db77d5c27fabc0b3b03876726b
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-12-12 00:06:07 +0900

    feat: 新たなイベントを設定

--- function_test.go
+++ function_test.go
@@ -116,7 +116,7 @@ func TestCountdownText2(t *testing.T) {
 func TestGenerateImage(t *testing.T) {
 	for i, imgInfo := range imageList {
 		t.Logf("%+v", imgInfo)
-		out := generateTodayImage(imgInfo, "2020/12/12\n「Don’t Blink」\n発売記念オンラインリリースイベントまで\nあと 18 日")
+		out := generateTodayImage(imgInfo, "2020/12/13\n「Don’t Blink」\n発売記念インターネットサイン会まで\nあと 18 日")
 		f, err := os.Create(fmt.Sprintf("./output%02d.png", i))
 		if err != nil {
 			t.Errorf("failed to save file")
