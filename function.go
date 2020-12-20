6abd742db7a361509e11659a991fd3175b99a882
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-12-21 02:16:20 +0900

    feat: 新しいイベントを設定

--- function.go
+++ function.go
@@ -147,12 +147,12 @@ func selectPOPSong(t time.Time) (SongInfo, string) {
 
 func getTargetDate() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 12, 13, 0, 0, 0, 0, jst)
+	return time.Date(2020, 12, 22, 0, 0, 0, 0, jst)
 }
 
 func getTargetDateTime() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 12, 13, 13, 0, 0, 0, jst)
+	return time.Date(2020, 12, 22, 15, 0, 0, 0, jst)
 }
 
 func getNow() time.Time {
@@ -311,12 +311,12 @@ func main() {
 	var text string
 	var textTw string
 	if near {
-		text = "まもなく\n「Don’t Blink」\n発売記念インターネットサイン会！"
-		textTw = "まもなく「Don’t Blink」 発売記念インターネットサイン会！"
+		text = "まもなく\nLuna FESTIVAL！"
+		textTw = "まもなくLuna FESTIVAL！"
 		out = generateTodayImage(selectRandomImage(), text)
 	} else {
-		text = fmt.Sprintf("2020/12/13\n「Don’t Blink」\n発売記念インターネットサイン会まで\n%s!!", countdownText(now))
-		textTw = fmt.Sprintf("2020/12/13 「Don’t Blink」 発売記念インターネットサイン会まで%s!!", countdownText(now))
+		text = fmt.Sprintf("2020/12/22\nLuna FESTIVALまで\n%s!!", countdownText(now))
+		textTw = fmt.Sprintf("2020/12/22 Luna FESTIVALまで%s!!", countdownText(now))
 		out = generateTodayImage(selectRandomImage(), text)
 	}
 	// encode image to base64
