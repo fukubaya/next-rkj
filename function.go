fcc755b1f87afcc4113139c6ba129944f5b532fb
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2020-12-23 01:31:06 +0900

    feat: 新しいイベントを設定

--- function.go
+++ function.go
@@ -147,12 +147,12 @@ func selectPOPSong(t time.Time) (SongInfo, string) {
 
 func getTargetDate() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 12, 22, 0, 0, 0, 0, jst)
+	return time.Date(2020, 12, 26, 0, 0, 0, 0, jst)
 }
 
 func getTargetDateTime() time.Time {
 	jst, _ := time.LoadLocation(location)
-	return time.Date(2020, 12, 22, 15, 0, 0, 0, jst)
+	return time.Date(2020, 12, 26, 17, 0, 0, 0, jst)
 }
 
 func getNow() time.Time {
@@ -311,12 +311,12 @@ func main() {
 	var text string
 	var textTw string
 	if near {
-		text = "まもなく\nLuna FESTIVAL！"
-		textTw = "まもなくLuna FESTIVAL！"
+		text = "まもなく\n1st single 「Don’t Blink」\n発売決定記念Talk Portトーク特典会！"
+		textTw = "まもなく1st single 「Don’t Blink」 発売決定記念Talk Portトーク特典会！"
 		out = generateTodayImage(selectRandomImage(), text)
 	} else {
-		text = fmt.Sprintf("2020/12/22\nLuna FESTIVALまで\n%s!!", countdownText(now))
-		textTw = fmt.Sprintf("2020/12/22 Luna FESTIVALまで%s!!", countdownText(now))
+		text = fmt.Sprintf("2020/12/26\n1st single 「Don’t Blink」\n発売決定記念Talk Portトーク特典会まで\n%s!!", countdownText(now))
+		textTw = fmt.Sprintf("2020/12/26 1st single 「Don’t Blink」 発売決定記念Talk Portトーク特典会まで%s!!", countdownText(now))
 		out = generateTodayImage(selectRandomImage(), text)
 	}
 	// encode image to base64
--- function_test.go
+++ function_test.go
@@ -116,7 +116,7 @@ func TestCountdownText2(t *testing.T) {
 func TestGenerateImage(t *testing.T) {
 	for i, imgInfo := range imageList {
 		t.Logf("%+v", imgInfo)
-		out := generateTodayImage(imgInfo, "2020/12/22\nLuna FESTIVALまで\nあと 18 日")
+		out := generateTodayImage(imgInfo, "2020/12/26\n1st single 「Don’t Blink」\n発売決定記念Talk Portトーク特典会まで\nあと 18 日")
 		f, err := os.Create(fmt.Sprintf("./output%02d.png", i))
 		if err != nil {
 			t.Errorf("failed to save file")
